f7c56d1af6a272da9fba849f57fa1369cda30a94
victor.dev <joaovictorbezerra.dev@gmail.com>
2026-04-18 18:51:36 -0400

    feat: configure Hilt application setup and AndroidManifest file

---
+++ app/src/main/java/io/github/victorbezerradev/connectifyflow/app/ConnectifyFlowApp.kt
@@ -0,0 +1,6 @@
+package io.github.victorbezerradev.connectifyflow.app
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ConnectifyFlowApp : Application()
